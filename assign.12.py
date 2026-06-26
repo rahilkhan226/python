fac4ba122131082d839801581703f35fe453dc5e
rahilkhan226 <khanr.k000007@gmail.com>
2026-06-26 22:53:34 +0530

    Assignment12

---
+++ assign.12.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance  
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if self.__balance >= amount:
+            self.__balance -= amount
+        else:
+            print("Insufficient funds")
+
+    def get_balance(self): 
+        return self.__balance
+
+
+account = BankAccount(1000)
+
+account.deposit(1000)
+
+account.withdraw(300)
+
+print(account.get_balance())
